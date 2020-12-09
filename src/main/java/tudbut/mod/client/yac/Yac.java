5ce6a037e244dda26edbaccfab27e64f65ebeae9
TudbuT <dh.danielhister@gmail.com>
2020-12-09 19:30:49 +0100

    fucked up some values

--- src/main/java/tudbut/mod/client/yac/Yac.java
+++ src/main/java/tudbut/mod/client/yac/Yac.java
@@ -23,8 +23,8 @@ import java.util.Map;
 @Mod(modid = Yac.MODID, name = Yac.NAME, version = Yac.VERSION)
 public class Yac {
     public static final String MODID = "yac";
-    public static final String NAME = "Yac Client";
-    public static final String VERSION = "1.0";
+    public static final String NAME = "YAC Client";
+    public static final String VERSION = "vB0.1.0";
     
     public static Module[] modules ;
     public static EntityPlayerSP player;
