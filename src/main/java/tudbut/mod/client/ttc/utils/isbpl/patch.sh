4724699dece0356551918b529ae390bc0b3f3978
TudbuT <tudbut@tudbut.de>
2022-05-14 16:14:31 +0200

    isbpl update

--- src/main/java/tudbut/mod/client/ttc/utils/isbpl/patch.sh
+++ src/main/java/tudbut/mod/client/ttc/utils/isbpl/patch.sh
@@ -1,6 +1,6 @@
 diff --horizon-lines=20 --unified ISBPL.java.original ISBPL.java > ISBPL.patch
 cat ISBPL.patch
 read
-cp ~/gitshit/isbpl/bootstrap/ISBPL.java ISBPL.java
-cp ~/gitshit/isbpl/bootstrap/ISBPL.java ISBPL.java.original
+cp ~/gitshit/isbpl/ISBPL.java ISBPL.java
+cp ~/gitshit/isbpl/ISBPL.java ISBPL.java.original
 patch ISBPL.java ISBPL.patch && rm ISBPL.patch
