94aa8d0811bb492f84d26e8a66323120d9be46b2
TudbuT <tudbut@tudbut.de>
2022-05-14 16:42:31 +0200

    new makerelease

--- makerelease.sh
+++ makerelease.sh
@@ -1,3 +1,13 @@
+cat version.txt
+echo " <- old version"
+read VERSION
+echo -n "$VERSION" > version.txt
+sed "s/$(cat previousVersion.txt)/$(cat version.txt)/g" < src/main/java/tudbut/mod/client/ttc/TTC.java > tmp.TTC.java
+sed "s/$(cat previousVersion.txt)/$(cat version.txt)/g" < src/main/resources/mcmod.info > tmp.mcmod.info
+mv tmp.mcmod.info src/main/resources/mcmod.info
+mv tmp.TTC.java src/main/java/tudbut/mod/client/ttc/TTC.java
+./gradlew jar
+git commit -m "makerelease.sh: set version" version.txt build src/main/java/tudbut/mod/client/ttc/TTC.java src/main/resources/mcmod.info
 cat > message.txt << EOF
 > $(cat version.txt)
 
@@ -18,6 +28,14 @@ EOF
 
 nvim message.txt
 
+xdg-open "https://github.com/tudbut/ttc/releases/new"
+
+git checkout master
+git merge dev
+read
 git push
 git tag -aF message.txt $(cat version.txt)
 git push --tags
+git checkout dev
+cp version.txt previousVersion.txt
+git commit -m "makerelease.sh: set previous version" previousVersion.txt
