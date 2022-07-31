221b1778b16fa960d117cb64c5690f790ba5ac7d
TudbuT <tudbut@tudbut.de>
2022-07-31 18:29:39 +0200

    fix user count

--- src/main/java/tudbut/mod/client/ttc/mods/Msg.java
+++ src/main/java/tudbut/mod/client/ttc/mods/Msg.java
@@ -15,7 +15,7 @@ public class Msg extends Module {
     @Override
     public void onEveryChat(String s, String[] args) {
         if(args.length == 0) {
-            ChatUtils.print("§aPlayers online: " + WebServices2.client.getUsersOnline());
+            ChatUtils.print("§aPlayers online: " + WebServices2.client.getUsersOnline().success(Integer.class).get());
             return;
         }
         String name = args[0];
